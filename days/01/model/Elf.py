e8e6f6316e15ab3330ce761a3a1cefb7fd9fe7dc
Evgeny <43423375+chesnoksatan@users.noreply.github.com>
2022-12-03 13:24:22 +0300

    delete some files

--- days/01/model/Elf.py
+++
@@ -1,8 +0,0 @@
-class Elf:
-    carried_provision: list[int] = []
-
-    def __init__(self, elf_provision: list[int]):
-        self.carried_provision = elf_provision
-
-    def all_carried_calories(self) -> int:
-        return sum(self.carried_provision)
